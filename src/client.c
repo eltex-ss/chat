e117fffec35622da16ac3100e308ea2fa3fae23f
Prichman <prichmen@gmail.com>
2016-07-12 19:39:04 +0600

    Done work with server, redesigned client. TODO: fix connect

--- src/client.c
+++ src/client.c
@@ -1,14 +1,31 @@
 #include "common.h"
 
-static int queue_id;
+static int server_queue;
+static int messages_queue;
+static char nickname[16];
 
-void OpenQueue(void)
+void OpenQueues(void)
 {
-  int queue_key = ftok("/home/Alexander/Projects/SummerSchool/chat/server.c", 1);
-  
-  queue_id = msgget(queue_key, 0666);
-  if (queue_id == -1) {
+  char path_to_file[] = "/home/Alexander/Projects/SummerSchool/chat/server.c";
+  key_t server_queue_key = ftok(path_to_file, 1);
+  key_t messages_queue_key = ftok(path_to_file, 2);
+
+  server_queue = msgget(server_queue_key, 0666);
+  if (server_queue == -1) {
     perror("Error, msgget\n");
+    exit(1);
+  }
+  messages_queue = msgget(messages_queue_key, 0666);
+  if (messages_queue == -1) {
+    perror("Error, msgget\n");
+    exit(1);
+  }
+}
+
+void SendMessage(struct Message *message)
+{
+  if (msgsnd(server_queue, message, messageMaxSize, 0) == -1) {
+    perror("Error, send message\n");
     exit(1);
   }
 }
@@ -16,17 +33,15 @@ void OpenQueue(void)
 void SendNickname(void)
 {
   struct Message message;
-  char *buf;
-  size_t buf_size;
+  char white_space;
 
+  printf("Choose nickname. Note that it must be <= 15 length\n");
   printf("Your nickname: ");
-  getline(&buf, &buf_size, stdin);
+  scanf("%s", nickname);
+  scanf("%c", &white_space);
   message.type = 1;
-  sprintf(message.text, "%s", buf);
-  if (msgsnd(queue_id, &message, messageMaxSize, 1) == -1) {
-    perror("Error, send nickname\n");
-    exit(1);
-  }
+  sprintf(message.text, "Connected new user: %s", nickname);
+  SendMessage(&message);
 }
 
 void ReadAndSend(void)
@@ -36,22 +51,60 @@ void ReadAndSend(void)
   struct Message message;
   
   message.type = 3;
-  while (1) {
+  while (message.type == 3) {
     printf("Write a message:\n");
     getline(&buf, &buf_size, stdin);
-    strcpy(message.text, buf);
-    if (msgsnd(queue_id, &message, messageMaxSize, 1) == -1) {
-      perror("Error, send message\n");
-      exit(1);
+    buf[strlen(buf)-1] = '\0';
+    if (buf_size > 2 && buf[0] == ':' && buf[1] =='q' ) {
+      message.type = 2;
+      sprintf(message.text, "User disconnected: %s", nickname);
     }
+    else {
+      sprintf(message.text, "%s: %s", nickname, buf);
+    }
+    SendMessage(&message);
+  }
+}
+
+void ReadMessages(void)
+{
+  struct Message message;
+
+  while (1) {
+    if (msgrcv(messages_queue, &message, messageMaxSize, 0, 0) == -1) {
+      perror("Error, msgrcv\n");
+    }
+    
+    if (message.text[0] == 'U')
+      return;
+
+    printf("%s\n", message.text);
   }
 }
 
 int main(void)
 {
-  OpenQueue();
+  int errorStatus;
+
+  OpenQueues();
   SendNickname();
-  ReadAndSend();
-  
+  if (fork() == 0) {
+    /*  Read input from keyboard and send to it to server */
+    close(stdout);
+    ReadAndSend();
+    exit(0);
+  }
+
+  if (fork() == 0) { 
+    /*  Read messages from server */
+    ReadMessages();
+    exit(0);
+  } else {
+    close(stdout);
+  }
+
+  for (int i = 0; i < 2; ++i)
+    wait(&errorStatus);
+
   return 0;
 }
--- src/server.c
+++ src/server.c
@@ -4,40 +4,105 @@
 #define MAX_USERS_COUNT 100
 #define MAX_USER_NAME_LENGHT 40
 
-void closeQueue(int queue_id)
-{
-  msgctl(queue_id, IPC_RMID, 0);
+static int server_queue;
+static int messages_queue;
+
+void CreateQueues(void)
+{ 
+  char path_to_file[] = "/home/Alexander/Projects/SummerSchool/chat/server.c";
+  key_t server_queue_key    = ftok(path_to_file, 1);
+  key_t messages_queue_key  = ftok(path_to_file, 2);
+
+  server_queue = msgget(server_queue_key, IPC_CREAT | 0666);
+  if (server_queue == -1) {
+    perror("Error, Can't create the queue\n");
+    exit(1);
+  }
+  messages_queue = msgget(messages_queue_key, IPC_CREAT | 0666);
+  if (messages_queue == -1) {
+    perror("Error, Can't create the queue\n");
+    exit(1);
+  }
 }
 
-void printList(struct List *head)
+void RemoveQueues(void)
 {
-  struct List *currentElem = head->next;
+  msgctl(server_queue, IPC_RMID, 0);
+  msgctl(messages_queue, IPC_RMID, 0);
+}
 
-  printf("List:\n");
-  while (currentElem) {
-    printf("%s\n", currentElem->username);
-    currentElem = currentElem->next;
+void SendMessage(struct Message *message)
+{
+  message->type = 1;
+  if (msgsnd(messages_queue, message, messageMaxSize, 0) == -1) {
+    perror("Error, msgsnd\n");
+  }
+}
+
+void StartReceiving(void)
+{ 
+  struct  Message message;
+  struct  List *usernames = CreateList();
+  /*  struct  List *user = NULL; */
+  
+  while (1) {
+    if (msgrcv(server_queue, &message, messageMaxSize, 0, 0) == -1) {
+      perror("Error, msgrcv\n");
+      msgctl(server_queue, IPC_RMID, 0);
+    }
+    switch (message.type) {
+      case 1:
+        /*  New user connected */
+        AddLeaf(message.text, usernames);
+        SendMessage(&message);
+        break;
+      case 2:
+        /*  User disconected */
+        RemoveLeafByName(message.text, usernames);
+        SendMessage(&message);
+        /*  TODO: close fork() and send notification */
+        break;
+      case 3:
+        /*  Send message to all */
+        /*  TODO: ^ */
+        SendMessage(&message);
+        break;
+    }
+
+    printf("%s\n", message.text);
   }
 }
 
 int main(void)
 {
-  int queue_key = ftok("/home/Alexander/Projects/SummerSchool/chat/server.c",
-                       1);
-  int queue_id = msgget(queue_key, IPC_CREAT | 0666);
-  struct Message message;
-  char user_names[MAX_USERS_COUNT][MAX_USER_NAME_LENGHT];
-  
-  if (queue_id == -1) {
-    printf("Error, msgget\n");
-  }
-  while (1) {
-    if (msgrcv(queue_id, &message, messageMaxSize, 1, 0) == -1) {
-      printf("Error, msgrcv\n");
-      msgctl(queue_id, IPC_RMID, 0);
-    }
-    printf("Message: %s\n", message.text);
+  char input, white_space;
+  int status;
+
+  CreateQueues();
+  pid_t receive_id = fork();
+  if (receive_id == 0) {
+    StartReceiving();
+    exit(0);
   }
 
+  if (fork() == 0) {
+    while (1) {
+      scanf("%c", &input);
+      scanf("%c", &white_space);
+      if (input =='q') {
+        break;
+      }
+      close(stdout);
+    }
+    kill(receive_id, SIGKILL);
+    exit(0);
+  } else {
+    close(stdout);
+  }
+  for (int i = 0; i < 2; ++i) {
+    wait(&status);
+  }
+
+  RemoveQueues();
   return 0;  
 }
